9de8ac2620a13e654e8ff7f167a41c3078c3289e
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-24 10:01:01 -0500

    added state to handle click and splay x or O depending  on the previous click

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,24 @@
+import { useState } from "react"
 import { Squares } from "./Squares"
 
 export const Board =()=>{
+const [squares, setSquares] =   useState(Array(9).fill(null))
     return (<>
 
       <div className="board-row">
-        <Squares value={1}/>
-        <Squares value={2}/>
-        <Squares value={3}/>
+        <Squares value={squares[0]}/>
+        <Squares value={squares[1]}/>
+        <Squares value={squares[2]}/>
       </div>
       <div className="board-row">
-        <Squares value={4}/>
-        <Squares value={5}/>
-        <Squares value={6}/>
+        <Squares value={squares[3]}/>
+        <Squares value={squares[4]}/>
+        <Squares value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Squares value={7}/>
-        <Squares value={8}/>
-        <Squares value={9}/>
+        <Squares value={squares[6]}/>
+        <Squares value={squares[7]}/>
+        <Squares value={squares[8]}/>
       </div>
     </>)
 }
