2dd1d3e7d8a2ac5b09318e16a81feeecb9f0905c
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-24 09:49:19 -0500

    displays values X  when click

--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -1,3 +1,12 @@
-export const Squares=({value})=>{
-    return <button>{value}</button>
+import { useState } from "react"
+
+export const Squares=()=>{
+   const [value, setValue] =  useState(null)
+
+const  handleClick = ()=>{
+    setValue("X")
+
+
+}
+    return <button className="square" onClick={handleClick}>{value}</button>
 }
