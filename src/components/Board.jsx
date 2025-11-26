19e29035fcf566dc4ae6c53c747880e73c23c774
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-25 23:35:40 -0500

    winner function is correctly updating on the screen

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,6 +6,59 @@ const [squares, setSquares] =   useState(Array(9).fill(null))
 const [xIsNext, setXIsNext] = useState(true)
 
 
+const winner =  winnerFunction(squares)
+
+  let status;
+  if(winner){
+    status= "Winner: "+ winner
+  }else{
+    status ="next player: "+ (xIsNext ? "X" : "O")
+  }
+
+
+
+ const handleClick =(i)=>{
+ 
+
+  
+  if(squares[i]){
+    return;; 
+  }
+  const nextSquares = squares.slice();
+  console.log('copy',nextSquares)
+  
+  if(xIsNext){
+    nextSquares[i] = "X"
+  }else{
+    nextSquares[i] = "O"
+  }
+   setSquares(nextSquares)
+   setXIsNext(!xIsNext)
+ }
+    return (<>
+       <h2 className="status" style={{color:"skyblue"}}>{status}</h2>
+       <div>
+      <div  className="board-row">
+        <Squares value={squares[0]} onSquareClick= { ()=> handleClick(0)} />
+        <Squares value={squares[1]} onSquareClick= { ()=> handleClick(1)}/>
+        <Squares value={squares[2]} onSquareClick= { ()=> handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Squares value={squares[3]} onSquareClick= { ()=> handleClick(3)}/>
+        <Squares value={squares[4]}onSquareClick= { ()=> handleClick(4)}/>
+        <Squares value={squares[5]} onSquareClick= { ()=> handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Squares value={squares[6]} onSquareClick= { ()=> handleClick(6)}/>
+        <Squares value={squares[7]} onSquareClick= { ()=> handleClick(7)}/>
+        <Squares value={squares[8]} onSquareClick= { ()=> handleClick(8)}/>
+      </div>
+      </div>
+    </>)
+}
+
+
+
 const  winnerFunction = (squares)=>{
     const winner = [
     [0, 1, 2],
@@ -31,42 +84,4 @@ const  winnerFunction = (squares)=>{
     }
     return null;
 }
-   winnerFunction(squares)
-
- const handleClick =(i)=>{
- 
-
   
-  if(squares[i]){
-    return;; 
-  }
-  const nextSquares = squares.slice();
-  console.log('copy',nextSquares)
-  
-  if(xIsNext){
-    nextSquares[i] = "X"
-  }else{
-    nextSquares[i] = "O"
-  }
-   setSquares(nextSquares)
-   setXIsNext(!xIsNext)
- }
-    return (<>
-
-      <div className="board-row">
-        <Squares value={squares[0]} onSquareClick= { ()=> handleClick(0)} />
-        <Squares value={squares[1]} onSquareClick= { ()=> handleClick(1)}/>
-        <Squares value={squares[2]} onSquareClick= { ()=> handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Squares value={squares[3]} onSquareClick= { ()=> handleClick(3)}/>
-        <Squares value={squares[4]}onSquareClick= { ()=> handleClick(4)}/>
-        <Squares value={squares[5]} onSquareClick= { ()=> handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Squares value={squares[6]} onSquareClick= { ()=> handleClick(6)}/>
-        <Squares value={squares[7]} onSquareClick= { ()=> handleClick(7)}/>
-        <Squares value={squares[8]} onSquareClick= { ()=> handleClick(8)}/>
-      </div>
-    </>)
-}
