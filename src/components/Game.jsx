e8fadaad9fce4f93911f1cab09a598a06813f9dc
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-26 08:42:05 -0500

    added move function to track the move history up to the current square we are visiting

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,25 +4,37 @@ import { Board } from "./Board"
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] =useState(0);
 
   //get the current squares history
-  const currentSquares = history[history.length-1]
+  // const currentSquares = history[history.length-1]
+  //
+  const currentSquares = history[currentMove]
   console.log(currentSquares)
 
 
   const handlePlay =(nextSquare) =>{
     console.log(nextSquare)
     // copy the current history and append next square 
-    // update the boolean xisNext 
-    setHistory([...history, nextSquare])
+    //setHistory([...history, nextSquare])
+
+    
+    //keep history up to the point or square we are viewing
+    //instead of using the whole squares length;
+    const nextHistory = [...history.slice(0, currentMove+1), nextSquare]
+    setHistory(nextHistory)
+
+    //each time we make a move, update current move 
+    //to point to latest history  entry
+    setCurrentMove(nextHistory.length-1)
   setXIsNext(!xIsNext)
   }
 
   //create moves using history 
-  const moves = history.map((squares, move)=>{
+  const moves = history.map((squares, i)=>{
     let description;
-    if(move >0){
-   description = "Go to next"+ move;
+    if(i >0){
+   description = "Go to next "+ i;
     }else{
       description ="Go to game start"
     }
@@ -30,9 +42,12 @@ export default function Game(){
 
     const jumpTo =(nextMove)=>{
      console.log(nextMove)
+     setCurrentMove(nextMove)
+     setXIsNext(nextMove%2===0)
     }
-    return <li>
-      <button onClick={()=>{jumpTo(move)}}>{description}</button>
+
+    return <li key={i}>
+      <button onClick={()=>{jumpTo(i)}}>{description}</button>
     </li>
   })
   
