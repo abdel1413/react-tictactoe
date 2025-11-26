7cfca411f6e9deafd720d77f350c42c107c9de2f
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-25 22:10:49 -0500

    added on click events and function to handle click

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,22 +3,39 @@ import { Squares } from "./Squares"
 
 export const Board =()=>{
 const [squares, setSquares] =   useState(Array(9).fill(null))
+const [xIsNext, setXIsNext] = useState(true)
+
+
+ const handleClick =(i)=>{
+  if(squares[i]){
+    return;; 
+  }
+  const nextSquare = squares.slice();
+  
+  if(xIsNext){
+    nextSquare[i] = "X"
+  }else{
+    nextSquare[i] = "O"
+  }
+   setSquares(nextSquare)
+   setXIsNext(!xIsNext)
+ }
     return (<>
 
       <div className="board-row">
-        <Squares value={squares[0]}/>
-        <Squares value={squares[1]}/>
-        <Squares value={squares[2]}/>
+        <Squares value={squares[0]} onSquareClick= {handleClick} />
+        <Squares value={squares[1]} onSquareClick= {handleClick}/>
+        <Squares value={squares[2]} onSquareClick= {handleClick}/>
       </div>
       <div className="board-row">
-        <Squares value={squares[3]}/>
-        <Squares value={squares[4]}/>
-        <Squares value={squares[5]}/>
+        <Squares value={squares[3]} onSquareClick= {handleClick}/>
+        <Squares value={squares[4]}onSquareClick= {handleClick}/>
+        <Squares value={squares[5]} onSquareClick= {handleClick}/>
       </div>
       <div className="board-row">
-        <Squares value={squares[6]}/>
-        <Squares value={squares[7]}/>
-        <Squares value={squares[8]}/>
+        <Squares value={squares[6]} onSquareClick= {handleClick}/>
+        <Squares value={squares[7]} onSquareClick= {handleClick}/>
+        <Squares value={squares[8]} onSquareClick= {handleClick}/>
       </div>
     </>)
 }
--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react"
 
-export const Squares=()=>{
+export const Squares=({onSquareClick})=>{
    const [value, setValue] =  useState(null)
 
-const  handleClick = ()=>{
-    setValue("X")
 
-
-}
-    return <button className="square" onClick={handleClick}>{value}</button>
+    return <button className="square" onClick={onSquareClick}>{value}</button>
 }
